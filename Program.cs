b7e15b1a84e0a6db7a6652eae434a3cefb29e32f
Deep Patel <deep.ag123007@gmail.com>
2019-07-10 10:55:53 -0500

    small task for morning

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace airplane_lighting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Boeing = new Plane()
+            {
+                Model = "139 privet",
+                Make = "Boeing",
+                Seats = 20,
+            };
+
+            Boeing.Fly();
+        }
+
+        class Plane
+        {
+
+
+            public string Make { get; set; }
+
+            public string Model { get; set; }
+
+            public double Seats { get; set; }
+
+            public void Fly()
+            {
+                Console.WriteLine($"{Model}: Fly for me because it's mine!");
+                Console.WriteLine($"{Make}: Made by Deep co.");
+                Console.WriteLine($"{Seats}: luxurios seats");
+            }
+
+        }
+
+
+    }
+}
